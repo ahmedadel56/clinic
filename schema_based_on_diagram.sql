50e7c795916aba8d3e2af434021f60ba605085c1
ahmedadel56 <ahmdadl563@gmail.com>
2021-12-14 23:05:25 +0200

    Fix syntax errors

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,19 +1,19 @@
 CREATE TABLE patients (
     id INT PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,
     name TEXT,
-    date_of_birth DATE,
+    date_of_birth DATE
 );
 CREATE TABLE medical_histories (
     id INT PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP,
-    patient_id DATE,
+    patient_id INT,
     status VARCHAR(20), 
-    CONSTRAINT patient_id FOREIGN KEY(patient_id) REFERENCES patients(id);
+    CONSTRAINT patient_id FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 CREATE TABLE treatments (
     id INT PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,
     type VARCHAR(20),
-    name VARCHAR(20),
+    name VARCHAR(20)
 );
 CREATE TABLE invoices (
     id INT PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,
@@ -21,7 +21,7 @@ CREATE TABLE invoices (
     generted_at TIMESTAMP,
     payed_at TIMESTAMP,
     medical_history_id INT,
-    CONSTRAINT medical_history_id FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id);
+    CONSTRAINT medical_history_id FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 
 );
 CREATE TABLE invoice_items (
@@ -31,7 +31,7 @@ CREATE TABLE invoice_items (
     total_price DECIMAL,
     invoice_id INT,
     treatment_id INT,
-    CONSTRAINT treatment_id FOREIGN KEY(treatment_id) REFERENCES treatments(id);
-    CONSTRAINT invoice_id FOREIGN KEY(invoice_id) REFERENCES invoices(id);
+    CONSTRAINT treatment_id FOREIGN KEY(treatment_id) REFERENCES treatments(id),
+    CONSTRAINT invoice_id FOREIGN KEY(invoice_id) REFERENCES invoices(id)
 
 );
